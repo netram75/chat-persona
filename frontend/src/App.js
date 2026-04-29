5f71eeb37971d29ff64c46f120541962bc067078
netram75 <netram.24bcs10329@sst.scaler.com>
2026-04-29 21:33:09 +0530

    fixed API endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
     setLoading(true);
 
-    const res = await fetch("http://localhost:5000/chat", {
+    const res = await fetch("https://chat-persona.onrender.com/chat",{
       method: "POST",
       headers: {
         "Content-Type": "application/json"
